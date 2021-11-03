dd363356897404a4e5f6eabe4abe51a25210eba6
Paul Smith <prsmith@ranken.edu>
2021-11-03 13:59:39 -0500

    hoisted state up to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
-import CartItem from './CartItem'
+import { useState } from 'react';
+import CartItem from './CartItem';
 
 function App() {
-  return (<div>
-    <h1>Shopping Cart</h1>
-    <CartItem name="Hat" quantity="2" />
-    <CartItem name="Tie" quantity="2" />
-    <CartItem name="Belt" quantity="1" />
-  </div>);
+  const [item1, setItem1] = useState({ id: 1, name: 'Hat', quantity: 2 });
+  const [item2, setItem2] = useState({ id: 2, name: 'Tie', quantity: 2 });
+  const [item3, setItem3] = useState({ id: 3, name: 'Belt', quantity: 1 });
+
+  function onNameChange(evt, item, setItem) {
+    const newName = evt.currentTarget.value;
+    setItem({ ...item, name: newName });
+    //item1.name = newName;
+    //setItem1(item1);
+    console.log('onNameChange', newName);
+  }
+
+  function onQuantityAdd(evt, item, setItem) {
+    const newQuantity = item.quantity + 1;
+    setItem({ ...item, quantity: newQuantity });
+    console.log('onQuantityAdd', newQuantity);
+  }
+
+  function onQuantityRemove(evt, item, setItem) {
+    const newQuantity = item.quantity - 1;
+    setItem({ ...item, quantity: newQuantity });
+    console.log('onQuantityAdd', newQuantity);
+  }
+
+  return (
+    <div>
+      <h1>Shopping Cart</h1>
+      <CartItem
+        item={item1}
+        onNameChange={(evt) => onNameChange(evt, item1, setItem1)}
+        onQuantityAdd={(evt) => onQuantityAdd(evt, item1, setItem1)}
+        onQuantityRemove={(evt) => onQuantityRemove(evt, item1, setItem1)}
+      />
+      <CartItem
+        item={item2}
+        onNameChange={(evt) => onNameChange(evt, item2, setItem2)}
+        onQuantityAdd={(evt) => onQuantityAdd(evt, item2, setItem2)}
+        onQuantityRemove={(evt) => onQuantityRemove(evt, item2, setItem2)}
+      />
+      <CartItem
+        item={item3}
+        onNameChange={(evt) => onNameChange(evt, item3, setItem3)}
+        onQuantityAdd={(evt) => onQuantityAdd(evt, item3, setItem3)}
+        onQuantityRemove={(evt) => onQuantityRemove(evt, item3, setItem3)}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,38 +1,51 @@
 import { useState } from 'react';
 
-function CartItem({ name, quantity }) {
+function CartItem({ item, onNameChange, onQuantityAdd, onQuantityRemove }) {
+  // const [ currentQuantity, setCurrentQuantity ] = useState(item.quantity);
+  // const [ currentName, setCurrentName ] = useState(item.name);
 
-  const [ currentQuantity, setCurrentQuantity ] = useState(parseInt(quantity));
-  const [ currentName, setCurrentName ] = useState(name);
+  // function onAdd(evt) {
+  //   console.log('add');
+  //   setCurrentQuantity(currentQuantity + 1);
+  // }
 
-  function onAdd(evt) {
-    console.log('add');
-    setCurrentQuantity(currentQuantity + 1);
-  }
+  // function onRemove(evt) {
+  //   console.log('remove');
+  //   setCurrentQuantity(currentQuantity - 1);
+  // }
 
-  function onRemove(evt) {
-    console.log('remove');
-    setCurrentQuantity(currentQuantity - 1);
-  }
-
-  function onNameChange(evt) {
-    const newName = evt.currentTarget.value;
-    setCurrentName(newName);
-    console.log('name change', newName);
-  }
+  // function onNameChange(evt) {
+  //   const newName = evt.currentTarget.value;
+  //   setCurrentName(newName);
+  //   console.log('name change', newName);
+  // }
 
   return (
-    <div className="CartItem d-flex">
-      <input value={currentName} className="CartItem-name form-control"
-             onChange={(evt) => onNameChange(evt)} />
-      <span className="CartItem-quantity m-3">{currentQuantity}</span>
-      <button type="button" className="CartItem-add btn btn-primary rounded-circle m-1"
-              onClick={(evt) => onAdd(evt)}>
+    <div id={`item-${item.id}`} className="CartItem d-flex">
+      <label htmlFor={`item-${item.id}-name`}>Name</label>
+      <input
+        id={`item-${item.id}-name`}
+        value={item.name}
+        className="CartItem-name form-control"
+        onChange={(evt) => onNameChange(evt)}
+      />
+      <label htmlFor={`item-${item.id}-quantity`}>Quantity</label>
+      <span id={`item-${item.id}-quantity`} className="CartItem-quantity m-3">
+        {item.quantity}
+      </span>
+      <button
+        type="button"
+        className="CartItem-add btn btn-primary rounded-circle m-1"
+        onClick={(evt) => onQuantityAdd(evt)}
+      >
         <span>+</span>
         <span className="visually-hidden">Add</span>
       </button>
-      <button type="button" className="CartItem-remove btn btn-danger rounded-circle m-1"
-              onClick={(evt) => onRemove(evt)}>
+      <button
+        type="button"
+        className="CartItem-remove btn btn-danger rounded-circle m-1"
+        onClick={(evt) => onQuantityRemove(evt)}
+      >
         <span>-</span>
         <span className="visually-hidden">remove</span>
       </button>
