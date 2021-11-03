bb568f4c9ecbb00eeabf72a3cee48cbdc38b83bb
Paul Smith <prsmith@ranken.edu>
2021-11-03 10:18:38 -0500

    hoisting status up

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,39 +1,16 @@
-import { useState } from 'react';
-
-function CartItem({ name, quantity }) {
-  const [currentQuantity, setCurrentQuantity] = useState(
-    parseInt(quantity) || 0
-  );
-  const [currentName, setCurrentName] = useState(name || '');
-
-  function onAdd(evt) {
-    console.log('add', currentQuantity);
-    setCurrentQuantity(currentQuantity + 1);
-  }
-
-  function onRemove(evt) {
-    console.log('remove', currentQuantity);
-    setCurrentQuantity(currentQuantity - 1);
-  }
-
-  function onNameChange(evt) {
-    console.log('change', evt.currentTarget.value);
-    setCurrentName(evt.currentTarget.value);
-  }
-
-  // Explain: value vs defaultValue
+function CartItem({ item, onNameChange, onQuantityAdd, onQuantityRemove }) {
   return (
     <div className="CartItem d-flex">
       <input
-        value={currentName}
+        value={item.name}
         className="CartItem-name form-control"
-        onChange={(evt) => onNameChange(evt)}
+        onChange={(evt) => onNameChange(evt, item)}
       />
-      <span className="CartItem-quantity m-3">{currentQuantity}</span>
+      <span className="CartItem-quantity m-3">{item.quantity}</span>
       <button
         type="button"
         className="CartItem-add btn btn-primary rounded-circle m-1"
-        onClick={(evt) => onAdd(evt)}
+        onClick={(evt) => onQuantityAdd(evt, item)}
       >
         <span>+</span>
         <span className="visually-hidden">Add</span>
@@ -41,7 +18,7 @@ function CartItem({ name, quantity }) {
       <button
         type="button"
         className="CartItem-remove btn btn-danger rounded-circle m-1"
-        onClick={(evt) => onRemove(evt)}
+        onClick={(evt) => onQuantityRemove(evt, item)}
       >
         <span>-</span>
         <span className="visually-hidden">remove</span>
